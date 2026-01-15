cf02b236d978f9b2cafd83ea0fd6a7c3279b3fd2
FEN_N3C <117320391+FEN-N3C@users.noreply.github.com>
2026-01-15 09:43:47 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,6 @@ local Config = {
     Enabled = false,
     TeamCheck = false,
     DeadCheck = false,
-    ForceFieldCheck = false,
     WallCheck = false,
     FriendCheck = false,
     Prediction = 0,
